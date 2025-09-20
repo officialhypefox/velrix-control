dfe11af8f3f3b5a4d3a69d3b37c10bfabb9c2d2c
Exotical <ItzExotical@gmail.com>
2025-09-20 14:34:28 +0200

    Fix/improve copyright notice

--- resources/views/filament/layouts/footer.blade.php
+++ resources/views/filament/layouts/footer.blade.php
@@ -1,9 +1,11 @@
-<footer class="flex flex-row items-center justify-center text-center space-x-1 p-4 text-gray-600 dark:text-gray-400">
+<footer class="flex flex-row flex-wrap items-center justify-center text-center space-x-1 p-4 text-gray-600 dark:text-gray-400">
     <span>&copy; {{ date('Y') }}</span>
-    <a class="font-semibold" href="https://hypefox.net" target="_blank">Hypefox Ltd</a>,
+    <span>·</span>
+    <a class="font-semibold" href="https://hypefox.net" target="_blank">Hypefox Ltd</a>
+    <span>&amp;</span>
     <a class="font-semibold" href="https://pelican.dev/docs/#core-team" target="_blank">Pelican</a>
     @if(config('app.debug'))
-        <div class="flex space-x-1 text-xs">
+        <div class="flex items-center space-x-1 text-xs ml-2">
             <x-filament::icon
                 :icon="'tabler-clock'"
                 @class(['w-4 h-4 text-gray-500 dark:text-gray-400'])
