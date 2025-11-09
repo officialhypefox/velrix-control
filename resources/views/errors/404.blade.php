1727dad9adc26263a467c5e006aa200f970d8059
badappearance <201956525+badappearance@users.noreply.github.com>
2025-11-09 12:56:14 +0000

    Keeping testing with deploy workflow

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @props([
 'code' => '404',
 'title' => 'Not found',
-'subtitle' => $exception->getMessage() ?? 'The requested resource was not found (test).',
+'subtitle' => $exception->getMessage() ?? 'The requested resource was not found.',
 'icon' => 'tabler-zoom-question'
 ])
 
--- resources/views/filament/layouts/footer.blade.php
+++ resources/views/filament/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="flex flex-row flex-wrap items-center justify-center text-center space-x-1 p-4 text-gray-600 dark:text-gray-400">
     <span>&copy; {{ date('Y') }}</span>
     <span>·</span>
-    <a class="font-semibold" href="https://hypefox.net" target="_blank">Hypefox Ltd</a>
+    <a class="font-semibold" href="https://hypefox.net" target="_blank">Hypefox Ltd (Test)</a>
     <span>&amp;</span>
     <a class="font-semibold" href="https://pelican.dev/docs/#core-team" target="_blank">Pelican</a>
     @if(config('app.debug'))
